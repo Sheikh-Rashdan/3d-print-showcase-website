aca6e09c59a318617f065db20fe57b8012758872
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-09-16 01:46:15 +0530

    Added a parameter to `getFilesUsingId` function to only return files of type image

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     const folder = folders.get(selectedFolder);
     if (!folder || folder.files !== null) return;
 
-    getFilesUsingId(folder.id, "createdTime desc").then((files) => {
+    getFilesUsingId(folder.id, "createdTime desc", true).then((files) => {
       setFolders((currentFolders) => {
         const nextFolders = new Map(currentFolders);
         nextFolders.set(selectedFolder, { ...folder, files });
@@ -48,15 +48,17 @@ async function getFolders(setFolders) {
 
 const filesRequestCache = new Map();
 
-async function getFilesUsingId(id, orderBy = "name") {
-  const cacheKey = `${id}:${orderBy}`;
+async function getFilesUsingId(id, orderBy = "name", imagesOnly = false) {
+  const cacheKey = `${id}:${orderBy}:${imagesOnly}`;
   if (filesRequestCache.has(cacheKey)) return filesRequestCache.get(cacheKey);
 
-  const url = `https://www.googleapis.com/drive/v3/files?key=AIzaSyCVDk734Nt4kQpEAO7vbsdwu73qQtA1iXw&q=%27${id}%27+in+parents&fields=files(id,name)&orderBy=${orderBy}`;
+  const url = `https://www.googleapis.com/drive/v3/files?key=AIzaSyCVDk734Nt4kQpEAO7vbsdwu73qQtA1iXw&q=%27${id}%27+in+parents&fields=files(id,name,mimeType)&orderBy=${orderBy}`;
   const request = fetch(url).then(async (response) => {
     if (!response.ok) throw new Error(`Google Drive request failed: ${response.status}`);
     const data = await response.json();
-    return data.files;
+    return imagesOnly
+      ? data.files.filter((file) => file.mimeType.startsWith("image"))
+      : data.files;
   });
 
   filesRequestCache.set(cacheKey, request);
